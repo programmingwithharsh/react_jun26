690e737aa08d80f3a715be027a8aebd1012b9432
Harsh Tiwari <harshtiwari343@email.com>
2026-06-17 13:03:34 +0530

    create username variable

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'font-awesome/css/font-awesome.min.css'
 
+const username = "Harsh AI Academy";
 createRoot(document.getElementById('root')).render(
-  "Harsh AI Academy"
+  username
 )
