4353c80d6b0a246e623f464a2938c1d4557ed234
Harsh Tiwari <harshtiwari343@email.com>
2026-06-17 12:30:36 +0530

    Added font awesome

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'font-awesome/css/font-awesome.min.css'
 
 createRoot(document.getElementById('root')).render(
   "Harsh AI Academy"
