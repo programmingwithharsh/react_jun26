a4c3c32e0b583e7f1e0946ff527c1ac11958865b
Harsh Tiwari <harshtiwari343@email.com>
2026-06-18 10:39:25 +0530

    Added constructor and componentDidMount lifecycle

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,18 @@ import Title from './Title';
 import Welcome from './Welcome';
 import Login from './Login';
 class App extends React.Component {
+
+    constructor() { // Mounting - component is created and displayed
+        super();
+        console.log("App constructor lifecycle 1");
+    }
+
+    componentDidMount() { // Mounting - component is created and displayed
+        console.log("App componentDidMount lifecycle 3");
+    }
+
     render() { // lifecycle
+        console.log("App render lifecycle 2");
         return (<div>
             <h1>This is App class Component</h1>
             <ProductList></ProductList>
