6060fe038dc969c9616dbdd2a87b91f764198cd4
Harsh Tiwari <harshtiwari343@email.com>
2026-06-19 12:13:33 +0530

    useNavigate hook

--- src/PageNotFound.jsx
+++ src/PageNotFound.jsx
@@ -1,6 +1,17 @@
+import { useNavigate, Link } from 'react-router-dom';
+
 const PageNotFound = () => {
+    const navigate = useNavigate(); // hooks
+
+    const goBack = () => {
+        navigate("/"); // redirect to home page
+    }
+
     return (<div>
         <h1>Page Not Found</h1>
+        <button className='btn btn-primary' onClick={goBack}>Go Back - useNavigate</button>
+        <hr />
+        <Link to="/">Go Back Link</Link>
     </div>);
 }
 
