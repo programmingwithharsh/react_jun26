71e6a824749fb6447f006fc32d13eb6605ab0a02
Harsh Tiwari <harshtiwari343@email.com>
2026-06-19 12:09:24 +0530

    useParams from url

--- src/ProductDetail.jsx
+++ src/ProductDetail.jsx
@@ -1,6 +1,11 @@
-const ProductDetail = () => {
+import { useParams } from 'react-router-dom';
+
+const ProductDetail = (props) => {
+    // Object Destructuring in ES6
+    let { id } = useParams();
+    console.log(props);
     return (<div>
-        <h1>Product Detail</h1>
+        <h1>Product Detail, id is {id}</h1>
     </div>);
 }
 
