f552bec2f0d8e92ff8ad3e4a5bb428fd59ca88c2
Harsh Tiwari <harshtiwari343@email.com>
2026-06-24 10:23:17 +0530

    Added bootstrap Grid

--- src/Bootstrap/CoreBootstrap.jsx
+++ src/Bootstrap/CoreBootstrap.jsx
@@ -2,14 +2,61 @@ import Carousel from "./Carousel";
 import Accordion from "./Accordion";
 import Spinner from "./Spinner";
 import ProgressBar from "./ProgressBar";
+import Button from 'react-bootstrap/Button';
+import Grid from "./Grid";
 
 function CoreBootstrap() {
     return (<>
-        <h1>Core Bootstrap Example</h1>
-        <Carousel />
-        <Accordion />
-        <Spinner />
-        <ProgressBar />
+        <div className="container">
+
+            <div className="row">
+                <div className="col-12">
+                    <Grid />
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-12">
+                    <h1>Core Bootstrap Example</h1>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-4">
+                    <button type="button">Without class Button</button> {/* Write own css code */}
+                </div>
+                <div className="col-4">
+                    <button type="button" class="btn btn-primary">With Class Button</button> {/* using bootstrap class for button */}
+                </div>
+                <div className="col-4">
+                    <Button variant="primary">React Bootstrap Module Button</Button> {/* Button component */}
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-12">
+                    <Carousel />
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-12">
+                    <Accordion />
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-12">
+                    <Spinner />
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-12">
+                    <ProgressBar />
+                </div>
+            </div>
+        </div>
     </>);
 }
 
---
+++ src/Bootstrap/Grid.jsx
@@ -0,0 +1,76 @@
+function Grid() {
+    return (<div>
+        <div className="container text-center">
+            <div className="row">
+                <div className="col-1 border">
+                    1 of 1
+                </div>
+                <div className="col-1 border">
+                    1 of 2
+                </div>
+                <div className="col-1 border">
+                    1 of 3
+                </div>
+                <div className="col-1 border">
+                    1 of 4
+                </div>
+                <div className="col-1 border">
+                    1 of 5
+                </div>
+                <div className="col-1 border">
+                    1 of 6
+                </div>
+                <div className="col-1 border">
+                    1 of 7
+                </div>
+                <div className="col-1 border">
+                    1 of 8
+                </div>
+                <div className="col-1 border">
+                    1 of 9
+                </div>
+                <div className="col-1 border">
+                    1 of 10
+                </div>
+                <div className="col-1 border">
+                    1 of 11
+                </div>
+                <div className="col-1 border">
+                    1 of 12
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-1 border">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam quibusdam repudiandae magnam eligendi nemo! Odit ex eveniet quis labore corporis iusto, sit delectus porro sunt! In vitae mollitia necessitatibus molestias?
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-6 border">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio laboriosam odio deleniti facilis quibusdam ipsum natus officiis tenetur blanditiis, aliquam aspernatur corporis repellat illum. Necessitatibus distinctio quam maxime laudantium quos.
+                </div>
+                <div className="col-6 border">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam laudantium a ex fugit. Culpa ea sequi perferendis architecto ipsa vero doloribus, dolorum reiciendis omnis nobis recusandae id aliquam dignissimos non.
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-3 border border-primary">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt optio voluptatum atque sequi eos laudantium quo rem fuga ea ab omnis culpa, repudiandae quia animi, voluptatem in nesciunt similique? Debitis?
+                </div>
+                <div className="col-3 border border-primary">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem iure eius laborum cumque maxime doloribus quaerat a odio autem eos nobis omnis quis voluptas, voluptates earum cum quisquam temporibus nemo?
+                </div>
+                <div className="col-3 border border-primary">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sapiente necessitatibus vero officiis! Doloremque nisi debitis ut harum non blanditiis quasi tenetur quis voluptate laborum cupiditate, ullam repellendus, beatae vero.
+                </div>
+                <div className="col-3 border border-primary">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sapiente necessitatibus vero officiis! Doloremque nisi debitis ut harum non blanditiis quasi tenetur quis voluptate laborum cupiditate, ullam repellendus, beatae vero.
+                </div>
+            </div>
+        </div>
+    </div>);
+}
+
+export default Grid;
